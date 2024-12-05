a79bb37ec37c1ab21d69987001cbb40afe3406c2
ManuelMeekattukulam <34407735+ManuelMeekattukulam@users.noreply.github.com>
2024-12-05 19:26:41 +0000

    Text chat duplicated

---
+++ VR_Library_1/Assets/Scripts/Databases/CatFacts.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Fusion;
+using TMPro;
+
+public class CatFacts : NetworkBehaviour
+{
+    public TMP_Text _messages;
+    public TMP_Text input;
+    public TMP_Text usernameInput;
+    public string username = "Default";
+
+    public void SetUsername()
+    {
+        username = usernameInput.text;
+    }
+
+    public void CallMessageRPC()
+    {
+        string message = input.text;
+        RPC_SendMessage(username, message);
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RPC_SendMessage(string username, string message, RpcInfo rpcInfo = default)
+    {
+        _messages.text += $"{username}: {message}\n";
+    }
+
+}
