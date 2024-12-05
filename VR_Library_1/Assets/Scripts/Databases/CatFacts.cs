45d9876fe2580b2ac6f3d482eeac4fd48cc993b3
ManuelMeekattukulam <34407735+ManuelMeekattukulam@users.noreply.github.com>
2024-12-05 19:51:32 +0000

    UI updated

--- VR_Library_1/Assets/Scripts/Databases/CatFacts.cs
+++ VR_Library_1/Assets/Scripts/Databases/CatFacts.cs
@@ -7,26 +7,27 @@ using TMPro;
 
 public class CatFacts : NetworkBehaviour
 {
+    private string cat_message ="Cat Message Here";
     public TMP_Text _messages;
-    public TMP_Text input;
-    public TMP_Text usernameInput;
-    public string username = "Default";
+    //public TMP_Text input;
+    //public TMP_Text usernameInput;
+    //public string username = "Default";
 
-    public void SetUsername()
-    {
-        username = usernameInput.text;
-    }
+    //public void SetUsername()
+    //{
+    //    username = usernameInput.text;
+    //}
 
     public void CallMessageRPC()
     {
-        string message = input.text;
-        RPC_SendMessage(username, message);
+        //string message = input.text;
+        RPC_SendMessage(cat_message);
     }
 
     [Rpc(RpcSources.All, RpcTargets.All)]
-    public void RPC_SendMessage(string username, string message, RpcInfo rpcInfo = default)
+    public void RPC_SendMessage(string message, RpcInfo rpcInfo = default)
     {
-        _messages.text += $"{username}: {message}\n";
+        _messages.text += $"{message}\n";
     }
 
 }
